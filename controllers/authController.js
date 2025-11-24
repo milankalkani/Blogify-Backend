0f4c12ba09992219825fe68e71b8730f505fb631
milankalkani <86038865+milankalkani@users.noreply.github.com>
2025-11-24 10:38:17 +0530

    Updated authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -78,6 +78,7 @@ exports.signupUser = async (req, res) => {
 
 // --- Login Controller ---
 exports.loginUser = async (req, res) => {
+  console.log(req.body);
   try {
     const { email, password } = req.body;
 
